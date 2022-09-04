4e58c0506970f7cbb338ecb24f1fa81c689becdc
pavan kothapally <85874797+kothapallypavan@users.noreply.github.com>
2022-09-05 01:23:47 +0530

    signup setup

--- frontendapp/src/App.js
+++ frontendapp/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Home from './components/home';
 import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import Loanform from './components/loanform';
+import Signup from './components/signup';
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home/>}></Route>
         <Route exact path="/loanform" element={<Loanform/>}></Route>
+        <Route exact path="/signup" element={<Signup/>}></Route>
       </Routes>
     </Router>
   );
