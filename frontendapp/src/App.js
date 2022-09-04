abc702a90a221d6528fa4fb99aeda95693cd9bcb
Ayesharuksar <112718529+Ayesharuksar@users.noreply.github.com>
2022-09-04 12:51:16 -0700

    loanform testing
    loanform testing

--- frontendapp/src/App.js
+++ frontendapp/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './components/home';
 import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import Loanform from './components/loanform';
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home/>}></Route>
+        <Route exact path="/loanform" element={<Loanform/>}></Route>
       </Routes>
     </Router>
   );
