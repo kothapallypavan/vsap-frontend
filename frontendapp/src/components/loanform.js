1e1fc1cf6d7095142d30fd5af462859025eff20f
pavan kothapally <85874797+kothapallypavan@users.noreply.github.com>
2022-09-05 01:37:41 +0530

    loan form
    loan form  options added

--- frontendapp/src/components/loanform.js
+++ frontendapp/src/components/loanform.js
@@ -5,20 +5,30 @@ function Loanform() {
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
+        <Form.Label>Email</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Loan type</Form.Label>
+        <Form.Select aria-label="Default select example">
+          <option>select loan type</option>
+          <option value="1">home</option>
+          <option value="2">personal</option>
+          <option value="3">car loan</option>
+        </Form.Select>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Loan period</Form.Label>
+        <Form.Select aria-label="Default select example">
+          <option>select loan duration</option>
+          <option value="1">10</option>
+          <option value="2">11</option>
+          <option value="3">12</option>
+        </Form.Select>
+      </Form.Group> 
+
       <Button variant="primary" type="submit">
         Submit
       </Button>
