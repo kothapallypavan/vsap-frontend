eccbb1792953c608e00d65f6276a9cb40180bcc5
vadapallisridivya1902 <70383283+vadapallisridivya1902@users.noreply.github.com>
2022-09-09 10:57:48 +0530

    Signup

--- frontendapp/src/components/signup.js
+++ frontendapp/src/components/signup.js
@@ -1,7 +1,52 @@
-function Signup() {
+
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+
+
+function Signup()
+{
+  
   return (
-    <div>signup form</div>
+       
+      <Card border="primary" style={{ width: '30rem',marginLeft:"500px",marginTop:"150px"}}>
+     
+      <Card.Body>
+        <Card.Title className="text-center" size="lg">SignUp</Card.Title>
+        
+        <FloatingLabel controlId="floatingPassword" label="First Name" className="mb-3">
+        <Form.Control type="text" placeholder="First Name" required />
+      </FloatingLabel>
+
+       
+      <FloatingLabel controlId="floatingPassword" label="Last Name" className="mb-3">
+        <Form.Control type="text" placeholder="Last Name" required/>
+      </FloatingLabel>
+
+        <FloatingLabel
+        controlId="floatingInput"
+        label="Email address"
+        className="mb-3">
+        <Form.Control type="email" placeholder="name@example.com"  required/>
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingPassword" label="Password" className="mb-3">
+        <Form.Control type="password" placeholder="Password" required/>
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingPassword" label="Confirm Password">
+        <Form.Control type="password" placeholder="Confirm Password" required/>
+      </FloatingLabel>
+   
+        <Button variant="primary" className="mt-3">Submit</Button>
+      </Card.Body>
+    </Card>
+   
+  
   );
 }
+
+
 
 export default Signup;
